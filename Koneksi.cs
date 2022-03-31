95a697a3807d2e9b055f44db234b98c99a0929cf
zefania020 <85860150+zefania020@users.noreply.github.com>
2022-03-31 12:19:51 +0700

    Create Koneksi.cs

---
+++ Koneksi.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//1
+using System.Data.SqlClient;
+
+namespace KasirBaru_Pertemuan7
+{
+	public class Koneksi
+	{
+		//2
+       	public SqlConnection GetConn() 
+       	{ 
+           	SqlConnection Conn = new SqlConnection(); 
+           	Conn.ConnectionString = "Data Source=LAPTOP-B2OT493M; initial catalog=Kasir; integrated security=true"; 
+           	return Conn; 
+       	} 
+	}
+}
